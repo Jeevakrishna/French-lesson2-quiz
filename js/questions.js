1751db67565f574c2be8bf39b162f8f091e433e0
Jeeva Krishna <88268605+Jeevakrishna@users.noreply.github.com>
2021-09-06 22:37:11 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,608 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+     {
+    numb: 1,
+    question: "Vidhya est en train de ______?",
+    answer: "Manger",
+    options: [
+      "faire le devoir",
+      "regarder la télé",
+      "faire la cuisine",
+      "Manger"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Vidhya lit ____ dans son portable?",
+    answer: "les messages",
+    options: [
+      "les messages",
+      "les méls",
+      "les lettres",
+      "les publicités"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Murthy est _____ de Sanjay ",
+    answer: "le grand-père",
+    options: [
+      "le père",
+      "le grand-père",
+      "le fils",
+      "le petit-fils"
+    ]
+  },
+    {
+    numb: 4,
+    question: "On gâte les enfants en les achetant_____?",
+    answer: "des gadgets",
+    options: [
+      "des bijoux",
+      "des jouets",
+      "des gadgets",
+      "des animaux domestiques"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Chez M.Murthy, on a ______",
+    answer: "un chat",
+    options: [
+      "un chien",
+      "un poisson",
+      "un jardin",
+      "un chat"
+    ]
+  },
+
+   {
+    numb: 6,
+    question: "Les grands-parents sont ______?",
+    answer: "au salon",
+    options: [
+      "à la cuisine",
+      "leur chambre",
+      "au salon",
+      "au balcon"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "______fait des devoirs?",
+    answer: "Sanjay",
+    options: [
+      "Vidhya",
+      "Sarita",
+      "Murthy",
+      "Sanjay"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Les grands-parents sont_____de la marche de la technologie?",
+    answer: "Mécontents",
+    options: [
+      "Contents",
+      "Mécontents",
+      "Indifférents",
+      "Acceptants"
+    ]
+  },
+
+ {
+    numb: 9,
+    question: "Monica a cassé.............?",
+    answer: "le bras",
+    options: [
+      "la jambe",
+      "le cou",
+      "le bras",
+      "le cheville"
+    ]
+  },
+
+
+ {
+    numb: 10,
+    question: " Il faut être........... quand on traversé al rue.?",
+    answer: "Prudent",
+    options: [
+      "Prudent",
+      "Négligeant",
+      "Charmant",
+      "Passionnant"
+    ]
+  },
+
+ {
+    numb: 11,
+    question: " Monica a glissé sur la peau de______?",
+    answer: "Banane",
+    options: [
+      "Mangue",
+      "Pomme",
+      "Banane",
+      "Raisin"
+    ]
+  },
+
+ {
+    numb: 12,
+    question: " Le peau de banane était_____le trottoir ?",
+    answer: "Sur",
+    options: [
+      "Sur",
+      "Sons",
+      "Sûr",
+      "Sans"
+    ]
+  },
+
+
+ {
+    numb: 13,
+    question: "Il fait le devoir en ______ la télé. ?",
+    answer: "Regardent",
+    options: [
+      "Regardes",
+      "Regardent",
+      "Regardez",
+      "Regarde"
+    ]
+  },
+
+ {
+    numb: 14,
+    question: "Elle mange en lisant les messages dans_____portable  ?",
+    answer: "Son",
+    options: [
+      "Sa",
+      "Son",
+      "Ses",
+      "le"
+    ]
+  },
+ {
+    numb: 15,
+    question: "Elle fait de la cuisine en parlant___téléphone",
+    answer: "au",
+    options: [
+      "à la",
+      "aux",
+      "au",
+      "à"
+    ]
+  },
+ {
+    numb: 16,
+    question: "Qu'est-ce qu'on_____faire ?",
+    answer: "peut",
+    options: [
+      "pouvoir",
+      "pu",
+      "peux",
+      "peut"
+    ]
+  },
+
+ {
+    numb: 17,
+    question: " On______à l'ère de la technologie ",
+    answer: "vit",
+    options: [
+      "vis",
+      "vivre",
+      "vit",
+      "vécu"
+    ]
+  },
+
+   {
+    numb: 18,
+    question: "Il n'y avait pas________portable.",
+    answer: "de",
+    options: [
+      "de",
+      "le",
+      "du",
+      "la"
+    ]
+  },
+
+ {
+    numb: 19,
+    question: " Nous______plus heureux ?.",
+    answer: "étions",
+    options: [
+      "étais",
+      "étions",
+      "était",
+      "étiez"
+    ]
+  },
+
+   {
+    numb: 20,
+    question: "En_______les gadgets ils gâtent les enfants?",
+    answer: "achetent",
+    options: [
+      "achètant",
+      "achetent",
+      "achetés",
+      "achète"
+    ]
+  },
+
+   {
+    numb: 21,
+    question: "Comment tu t'______cassé le bras ?",
+    answer: "es",
+    options: [
+      "es",
+      "suis",
+      "est",
+      "ai"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "Je________dans la rue?",
+    answer: "Marchais",
+    options: [
+      "Marchait",
+      "Marches",
+      "Marchais",
+      "Marchaient"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Je_____tombée?",
+    answer: "Suis",
+    options: [
+      "Suis",
+      "Est",
+      "Ai",
+      "Es"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Je lis les messages sur_______portable?",
+    answer: "Mon",
+    options: [
+      "Ma",
+      "Les",
+      "Mes",
+      "Mon"
+    ]
+  },
+
+   {
+    numb: 25,
+    question: "Je n'ai pas remarqué______peau de banane",
+    answer: "la",
+    options: [
+      "le",
+      "la",
+      "les",
+      "un"
+    ]
+  },
+
+   {
+    numb: 26,
+    question: "Tu______être prudent ?",
+    answer: "devais",
+    options: [
+      "devait",
+      "devais",
+      "devaient",
+      "doivent"
+    ]
+  },
+
+   {
+    numb: 27,
+    question: "Nous nous sommes______",
+    answer: "promenés",
+    options: [
+      "promènent",
+      "promenons",
+      "promène",
+      "promenés"
+    ]
+  },
+
+
+  {
+    numb: 28,
+    question: "Nous nous_____promenés ?",
+    answer: "sommes",
+    options: [
+      "avez",
+      "sommes",
+      "avons",
+      "ai"
+    ]
+  },
+
+
+   {
+    numb: 29,
+    question: "Il reste du gâteau. ? Il n'y a plus_____gâteau?",
+    answer: "de",
+    options: [
+      "de",
+      "des",
+      "d'",
+      "du"
+    ]
+  },
+  
+   {
+    numb: 30,
+    question: "Les filles sont aussi______que les garçons",
+    answer: "intelligentes",
+    options: [
+      "intelligents",
+      "intelligent",
+      "intelligentes",
+      "intelligente"
+    ]
+  },
+
+
+
+  {
+    numb: 31,
+    question: "Nous______le devoir ",
+    answer: "commencions",
+    options: [
+      "commencions",
+      "commençions",
+      "commences",
+      "commence"
+    ]
+  },
+
+   {
+    numb: 32,
+    question: "Tu_____du pain",
+    answer: "mangeais",
+    options: [
+      "mangais",
+      "mangeais",
+      "mange",
+      "mangent"
+    ]
+  },
+
+   {
+    numb: 33,
+    question: "Il_____le papier ?",
+    answer: "Jetait",
+    options: [
+      "Jettait",
+      "Jesuis",
+      "Jetait",
+      "Jeter"
+    ]
+  },
+
+   {
+    numb: 34,
+    question: " Nous_____des fruits ",
+    answer: "Mangions",
+    options: [
+      "Manges",
+      "Mangions",
+      "Mangeions",
+      "Mangeaient"
+    ]
+  },
+
+   {
+    numb: 35,
+    question: "Marie est plus_____que Mare ! ",
+    answer: "Gentille",
+    options: [
+      "Gentil",
+      "gentilé",
+      "Gentille",
+      "Gentils"
+    ]
+  },
+
+   {
+    numb: 36,
+    question: "Elle est ______ ?",
+    answer: "Prudente",
+    options: [
+      "Prudent",
+      "Prudente",
+      "Prudentte",
+      "Prudents"
+    ]
+  },
+ 
+   {
+    numb: 37,
+    question: "Les femmes sont ______! ",
+    answer: "Heureuses",
+    options: [
+      "Heureux",
+      "Heureu",
+      "Heureuse",
+      "Heureuses"
+    ]
+  },
+
+   {
+    numb: 38,
+    question: "Tu prépares..........concours ? ",
+    answer: "quel",
+    options: [
+      "quel",
+      "quels",
+      "quelle",
+      "quelles"
+     ] 
+  },
+
+   {
+    numb: 39,
+    question: " Il faut que tu ......... le devoir ",
+    answer: "finisses",
+    options: [
+      "finis",
+      "finisses",
+      "finir",
+      "finit"
+    ]
+  },
+
+   {
+    numb: 40,
+    question: " J'ai appris...... belle chanson tamoule ?",
+    answer: "de",
+    options: [
+      "un",
+      "une",
+      "de",
+      "de la"
+    ]
+  },
+   {
+    numb: 41,
+    question: " Vous.......combien, la semaine dernière  ?",
+    answer: "étiez",
+    options: [
+      "êtes",
+      "étiez",
+      "avez",
+      "serez"
+    ]
+  },
+   {
+    numb: 42,
+    question: " Combien coûte.......robe ?",
+    answer: "cette",
+    options: [
+      "ce",
+      "ces",
+      "cet",
+      "cette"
+    ]
+  },
+   {
+    numb: 43,
+    question: " La météo annonce de fortes pluies..... la semaine prochaine.!",
+    answer: "toute",
+    options: [
+      "tout",
+      "tous",
+      "toute",
+      "toutes"
+    ]
+  },
+  
+   {
+    numb: 44,
+    question: "Il emmène les élèves.........France",
+    answer: "en",
+    options: [
+      "à",
+      "à la",
+      "en",
+      "au"
+    ]
+  },
+   {
+    numb: 45,
+    question: " Nous nettoyons........jardin ?",
+    answer: "toutes",
+    options: [
+      "tous",
+      "tout ",
+      "toute",
+      "toutes"
+    ]
+  },
+   {
+    numb: 46,
+    question: " Tu prépares.........concours ?",
+    answer: "quel",
+    options: [
+      "quel",
+      "quels",
+      "quelle",
+      "quelles"
+    ]
+  },
+   {
+    numb: 47,
+    question: "Le ministre habite un quartier......est beau. ",
+    answer: "qui",
+    options: [
+      "que",
+      "qui",
+      "où",
+      "dont"
+    ]
+  },
+   {
+    numb: 48,
+    question: "Sanjay fait ....... devoir en regardant la télé ?",
+    answer:"son",
+    options: [
+      "sa",
+      "son",
+      "ses",
+      "aux"
+    ]
+  },
+   {
+    numb: 49,
+    question: "Vous habitez toujours....... banlieue ?",
+    answer: "au",
+    options: [
+      "au",
+      "à la",
+      "à",
+      "en"
+    ]
+  },
+   {
+    numb: 50,
+    question: "Tu rencontres......amis ?",
+    answer: "tes",
+    options: [
+      "ta",
+      "ton",
+      "tes",
+      "son"
+    ]
+  },
+ 
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
